1b48009baa371f27eb7d8d14308aee9d5ea40145
Damian Silvera <damiansebastians@gmail.com>
2023-09-08 12:39:29 +0200

    Contact OK

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Carbon;
 use Ramsey\Uuid\Uuid;
 
@@ -16,23 +17,29 @@ class ContactController extends Controller
 
     public function store(Request $request)
     {
-        if (filter_var($request->contact)) {
+        $validator = Validator::make($request->all(), [
+            'customer' => 'required',
+            'email' => 'required|email',
+            'contact' => 'required|numeric',
+            'comment' => 'required',
+        ]);
 
-            $contact = new Contact();
-
-            $contact->_id = Uuid::uuid4()->toString();
-            $contact->customer = trim(htmlspecialchars($request->customer));
-            $contact->date = Carbon::now()->toString();
-            $contact->email = trim(htmlspecialchars($request->email));
-            $contact->contact = trim(htmlspecialchars($request->contact));
-            $contact->comment = trim(htmlspecialchars($request->comment));
-            $contact->action = "Active";
-
-            $contact->save();
-
-            return redirect()->back()->with('success', 'Your form has been successfully submitted, and we will get back to you shortly');
-        } else {
-            return redirect()->back()->with('error', 'Invalid inputs');
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
         }
+
+        $contact = new Contact();
+        $contact->_id = Uuid::uuid4()->toString();
+        $contact->customer = trim(htmlspecialchars($request->customer));
+        $contact->date = Carbon::now();
+        $contact->email = trim(htmlspecialchars($request->email));
+        $contact->contact = trim(htmlspecialchars($request->contact));
+        $contact->comment = trim(htmlspecialchars($request->comment));
+        $contact->action = "Active";
+
+        $contact->save();
+
+        return redirect()->back()->with('success', 'Your form has been successfully submitted, and we will get back to you shortly');
     }
+
 }
