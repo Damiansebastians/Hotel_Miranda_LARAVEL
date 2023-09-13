e6da4ff5360a1d97d5373c9ec28d1976d32fa52d
Damian Silvera <damiansebastians@gmail.com>
2023-09-13 17:36:42 +0200

    add auth

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AboutUsController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\OffersController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoomController;
 
 
@@ -19,5 +19,23 @@ Route::get('/aboutUs', [AboutUsController::class, 'index']);
 
 Route::get("/offers", [OffersController::class, "index"]);
 
-Route::get('/contact',[ContactController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
 Route::post('/contact', [ContactController::class, 'store']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+
+require __DIR__ . '/auth.php';
